b16a5a5d073cc4635e1dd2151a36449678a51f0c
Daniel Pommer <daniel.pommer@protonmail.com>
2024-08-01 19:03:53 +0200

    added documentation and undistort frame

--- src/interacting_maps.h
+++ src/interacting_maps.h
@@ -31,16 +31,61 @@ struct Parameters{
 
 std::string  create_folder_and_update_gitignore(const std::string& foldername);
 
-void read_calib(const std::string file, std::vector<float>& calibration_data);
+/**
+ * @brief Reads the camera calibration file from a .txt-file.
+ * 
+ * @param file_path Path to file
+ * @param calibration_data Read out calibration data as float std::vector
+ */
+void read_calib(const std::string file_path, std::vector<float>& calibration_data);
 
+/**
+ * @brief Read the events from a .txt-file.
+ * 
+ * @param file_path path to file
+ * @param events Read out events as a std::vector of Events
+ * @param start_time Starting point from which events are considered
+ * @param end_time Ending point from which events are considered
+ * @param max_events maximum number of events to consider, overwrites time 
+ */
 void read_events(const std::string file_path, std::vector<Event>& events, float start_time, float end_time, int max_events);
 
-std::vector<std::vector<Event>> bin_events(std::vector<Event>& events, float bucketsize);
+/**
+ * @brief Put events into bins which form the frames for the interacting maps algorithm
+ * 
+ * @param events std::vector of Events to put into bins
+ * @param bin_size Size of the bins to put the events into, in seconds
+ * @return std::vector<std::vector<Event>> binned Events
+ */
+std::vector<std::vector<Event>> bin_events(std::vector<Event>& events, float bin_size);
 
-void create_frames(std::vector<std::vector<Event>>& bucketed_events, std::vector<Tensor<float,2>>& frames, int camera_height, int camera_width);
+/**
+ * @brief Creates frames out of the event information
+ * 
+ * @param binned_events Binned Events
+ * @param frames output
+ * @param camera_height number of pixels in x direction
+ * @param camera_width number of pixels in y direction
+ */
+void create_frames(std::vector<std::vector<Event>>& binned_events, std::vector<Tensor<float,2>>& frames, int camera_height, int camera_width);
 
+/**
+ * @brief Create a sparse matrix from temporal derivative data (Events) for the update function update_R_from_F
+ * 
+ * @param N Number of pixels (rows*cols)
+ * @param V temporal derivative matrix
+ * @param result sparse matrix with N*6 entries and dimensions N*3xN+3
+ */
 void create_sparse_matrix(int N, Tensor<float,2>& V, SpMat& result);
 
+/**
+ * @brief Undo lens distortion 
+ * 
+ * @param image distorted image 
+ * @param camera_matrix camera matrix obtained from read_calib (intrinsic to camera, focal length and optical centers)
+ * @param distortion_parameters distortion paramaters obtained from read_calib (radial distortion and tangential distortion)
+ * @return cv::Mat undistorted openCV image
+ */
 cv::Mat undistort_image(const cv::Mat& image, const cv::Mat& camera_matrix, const cv::Mat& distortion_parameters);
 
 // void undistort_images(std::vector<Tensor<float,2>>& images, Tensor<float,2> camera_matrix, std::vector<float> distortion_parameters, int height, int width);
