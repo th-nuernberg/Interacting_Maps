94ca631b2a0a9ca9a0eff0e29d3bf486dce5a65f
Daniel Pommer <daniel.pommer@protonmail.com>
2024-08-19 16:01:21 +0200

    added some consts and &

--- include/interacting_maps.h
+++ include/interacting_maps.h
@@ -8,6 +8,9 @@
 #include <opencv2/opencv.hpp>
 #include <autodiff/forward/real.hpp>
 #include <autodiff/forward/real/eigen.hpp>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 using namespace Eigen;
 
@@ -32,7 +35,7 @@ struct Parameters{
     float view_angle_y;
 };
 
-std::string  create_folder_and_update_gitignore(const std::string& foldername);
+fs::path create_folder_and_update_gitignore(const std::string& foldername);
 
 /**
  * @brief Reads the camera calibration file from a .txt-file.
@@ -40,7 +43,7 @@ std::string  create_folder_and_update_gitignore(const std::string& foldername);
  * @param file_path Path to file
  * @param calibration_data Read out calibration data as float std::vector
  */
-void read_calib(const std::string file_path, std::vector<float>& calibration_data);
+void read_calib(const std::string& file_path, std::vector<float>& calibration_data);
 
 /**
  * @brief Read the events from a .txt-file.
@@ -51,7 +54,7 @@ void read_calib(const std::string file_path, std::vector<float>& calibration_dat
  * @param end_time Ending point from which events are considered
  * @param max_events maximum number of events to consider, overwrites time 
  */
-void read_events(const std::string file_path, std::vector<Event>& events, float start_time, float end_time, int max_events);
+void read_events(const std::string& file_path, std::vector<Event>& events, float start_time, float end_time, int max_events);
 
 /**
  * @brief Put events into bins which form the frames for the interacting maps algorithm
@@ -70,7 +73,7 @@ std::vector<std::vector<Event>> bin_events(std::vector<Event>& events, float bin
  * @param camera_height number of pixels in x direction
  * @param camera_width number of pixels in y direction
  */
-void create_frames(std::vector<std::vector<Event>>& binned_events, std::vector<Tensor<float,2>>& frames, int camera_height, int camera_width);
+void create_frames(const std::vector<std::vector<Event>>& binned_events, std::vector<Tensor<float,2,Eigen::RowMajor>>& frames, const int camera_height, const int camera_width);
 
 /**
  * @brief Create a sparse matrix from temporal derivative data (Events) for the update function update_R_from_F
@@ -79,7 +82,7 @@ void create_frames(std::vector<std::vector<Event>>& binned_events, std::vector<T
  * @param V temporal derivative matrix
  * @param result sparse matrix with N*6 entries and dimensions N*3xN+3
  */
-void create_sparse_matrix(int N, Tensor<float,2>& V, SpMat& result);
+void create_sparse_matrix(const int N, const Tensor<float,2,Eigen::RowMajor>& V, SpMat& result);
 
 /**
  * @brief Undo lens distortion 
@@ -91,9 +94,9 @@ void create_sparse_matrix(int N, Tensor<float,2>& V, SpMat& result);
  */
 cv::Mat undistort_image(const cv::Mat& image, const cv::Mat& camera_matrix, const cv::Mat& distortion_parameters);
 
-// void undistort_images(std::vector<Tensor<float,2>>& images, Tensor<float,2> camera_matrix, std::vector<float> distortion_parameters, int height, int width);
+// void undistort_images(std::vector<Tensor<float,2,Eigen::RowMajor>>& images, Tensor<float,2,Eigen::RowMajor> camera_matrix, std::vector<float> distortion_parameters, int height, int width);
 
-// void undistort_frames(std::vector<Tensor<float,2>>& frames, Tensor<float,2> camera_matrix, std::vector<float> distortion_parameters, int height, int width);
+// void undistort_frames(std::vector<Tensor<float,2,Eigen::RowMajor>>& frames, Tensor<float,2,Eigen::RowMajor> camera_matrix, std::vector<float> distortion_parameters, int height, int width);
 
 cv::Mat frame2grayscale(const Eigen::MatrixXf& frame);
 
@@ -101,34 +104,32 @@ cv::Mat V2image(const Eigen::MatrixXf& V);
 
 cv::Mat vector_field2image(const Eigen::Tensor<float, 3>& vector_field);
 
-void create_VIFG_image(Tensor<float,2>& V, Tensor<float,2>& I, Tensor<float,3>& F, Tensor<float,3>& G, Tensor<float,2>& VIFG);
+void create_VIFG_image(Tensor<float,2,Eigen::RowMajor>& V, Tensor<float,2,Eigen::RowMajor>& I, Tensor<float,3,Eigen::RowMajor>& F, Tensor<float,3,Eigen::RowMajor>& G, Tensor<float,2,Eigen::RowMajor>& VIFG);
 
-// void find_C(int N_x, int N_y, float view_angle_x, float view_angle_y, float rs, Tensor<float,3>& C, Tensor<float,3>& dxdC, Tensor<float,3>& dydC);
-void find_C(int N_x, int N_y, float view_angle_x, float view_angle_y, float rs, Tensor<float,3>& CCM, Tensor<float,3>& C_x, Tensor<float,3>& C_y);
+// void find_C(int N_x, int N_y, float view_angle_x, float view_angle_y, float rs, Tensor<float,3,Eigen::RowMajor>& C, Tensor<float,3,Eigen::RowMajor>& dxdC, Tensor<float,3,Eigen::RowMajor>& dydC);
+void find_C(int N_x, int N_y, float view_angle_x, float view_angle_y, float rs, Tensor<float,3,Eigen::RowMajor>& CCM, Tensor<float,3,Eigen::RowMajor>& C_x, Tensor<float,3,Eigen::RowMajor>& C_y);
 
-void update_F_from_G(Tensor<float,3>& F, Tensor<float,2>& V, Tensor<float,3>& G, float lr, float weight_FG);
+void update_F_from_G(Tensor<float,3,Eigen::RowMajor>& F, Tensor<float,2,Eigen::RowMajor>& V, Tensor<float,3,Eigen::RowMajor>& G, float lr, float weight_FG);
 
-void update_G_from_F(Tensor<float,3>& G, Tensor<float,2>& V, Tensor<float,3>& F, float lr, float weight_GF);
+void update_G_from_F(Tensor<float,3,Eigen::RowMajor>& G, Tensor<float,2,Eigen::RowMajor>& V, Tensor<float,3,Eigen::RowMajor>& F, float lr, float weight_GF);
 
-void update_G_from_I(Tensor<float,3>& G, Tensor<float,3>& I_gradient, float weight_GI);
+void update_G_from_I(Tensor<float,3,Eigen::RowMajor>& G, Tensor<float,3,Eigen::RowMajor>& I_gradient, float weight_GI);
 
-void update_I_from_V(Tensor<float,2>& I, Tensor<float,2>& cum_V, float weight_IV, float time_step);
+void update_I_from_V(Tensor<float,2,Eigen::RowMajor>& I, Tensor<float,2,Eigen::RowMajor>& cum_V, float weight_IV, float time_step);
 
-void update_I_from_G(Tensor<float,2>& I, Tensor<float,3>& I_gradient, Tensor<float,3>& G, float weight_IG);
+void update_I_from_G(Tensor<float,2,Eigen::RowMajor>& I, Tensor<float,3,Eigen::RowMajor>& I_gradient, Tensor<float,3,Eigen::RowMajor>& G, float weight_IG);
 
-void update_F_from_R(Tensor<float,3>& F, Tensor<float,3>& C, Tensor<float,3>& Cx, Tensor<float,3>& Cy, Tensor<float,1> R, float weight_FR);
+void update_F_from_R(Tensor<float,3,Eigen::RowMajor>& F, Tensor<float,3,Eigen::RowMajor>& C, Tensor<float,3,Eigen::RowMajor>& Cx, Tensor<float,3,Eigen::RowMajor>& Cy, Tensor<float,1> R, float weight_FR);
 
-void update_R_from_F(Tensor<float,1> R, Tensor<float,3>& F, Tensor<float,3>& C, Tensor<float,3>& Cx, Tensor<float,3>& Cy, float weight_RF, int N);
+void update_R_from_F(Tensor<float,1> R, Tensor<float,3,Eigen::RowMajor>& F, Tensor<float,3,Eigen::RowMajor>& C, Tensor<float,3,Eigen::RowMajor>& Cx, Tensor<float,3,Eigen::RowMajor>& Cy, float weight_RF, int N);
 
-void vector_distance(Tensor<float,3> &vec1, Tensor<float,3> &vec2, Tensor<float,2> &distance);
+void vector_distance(const Tensor<float,3,Eigen::RowMajor> &vec1, const Tensor<float,3,Eigen::RowMajor> &vec2, Tensor<float,2,Eigen::RowMajor> &distance);
 
-void m23(Tensor<float,3>& In, Tensor<float,3>& Cx, Tensor<float,3>& Cy, Tensor<float,3>& Out);
+void m32(const Tensor<float,3,Eigen::RowMajor>& In, const Tensor<float,3,Eigen::RowMajor>& Cx, const Tensor<float,3,Eigen::RowMajor>& Cy, Tensor<float,3,Eigen::RowMajor>& Out);
 
-void m32(Tensor<float,3>& In, Tensor<float,3>& Cx, Tensor<float,3>& Cy, Tensor<float,3>& Out);
+void interacting_maps_step(Tensor<float,2,Eigen::RowMajor>& V, Tensor<float,2,Eigen::RowMajor>& cum_V, Tensor<float,2,Eigen::RowMajor>& I, Tensor<float,3,Eigen::RowMajor>& F, Tensor<float,3,Eigen::RowMajor>& G, Tensor<float,1>& R, const Tensor<float,3,Eigen::RowMajor>& CCM, const Tensor<float,3,Eigen::RowMajor>& dCdx, const Tensor<float,3,Eigen::RowMajor>& dCdy, SpMat& sparse_m, std::unordered_map<std::string,float>& weights, std::vector<int>& permutation, const int N);
 
-void interacting_maps_step(Tensor<float,2>& V, Tensor<float,2>& cum_V, Tensor<float,2>& I, Tensor<float,3>& F, Tensor<float,3>& G, Tensor<float,1>& R, Tensor<float,3>& CCM, Tensor<float,3>& dCdx, Tensor<float,3>& dCdy, std::unordered_map<std::string,float> weights, std::vector<int> permutation, int N);
-
-void interacting_maps(std::vector<Tensor<float,2>>& Vs, std::vector<Tensor<float,2>>& cum_Vs, std::unordered_map<std::string,float> weights, int iterations, std::string results_directory);
+void interacting_maps(std::vector<Tensor<float,2,Eigen::RowMajor>>& Vs, std::vector<Tensor<float,2,Eigen::RowMajor>>& cum_Vs, std::unordered_map<std::string,float> weights, int iterations, std::string results_directory);
 
 
 
