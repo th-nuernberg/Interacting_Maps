4dd2fb450bd00d1a708710edb78d117fe6223249
Daniel Pommer <daniel.pommer@th-nuernberg.de>
2025-08-22 12:03:13 +0200

    boost import works

---
+++ scripts/rebuild.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /opt/petalinux/2022.2/environment-setup-cortexa72-cortexa53-xilinx-linux
+cmake --build build
--- toolchain-gcc13.cmake
+++ toolchain-gcc13.cmake
@@ -1,10 +1,21 @@
-# Toolchain file for forcing GCC 13
+# toolchain-gcc13.cmake - cross compile for AArch64 SoC
 
-# C compiler
-set(CMAKE_C_COMPILER /usr/bin/gcc-13)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-# C++ compiler
-set(CMAKE_CXX_COMPILER /usr/bin/g++-13)
+# Sysroot from PetaLinux SDK
+set(CMAKE_SYSROOT /opt/petalinux/2022.2/sysroots/cortexa72-cortexa53-xilinx-linux)
 
-# (Optional) Fortran compiler, if you use it
-#set(CMAKE_Fortran_COMPILER /usr/bin/gfortran-13)
+# Cross compilers provided by the environment script
+set(CMAKE_C_COMPILER   aarch64-xilinx-linux-gcc)
+set(CMAKE_CXX_COMPILER aarch64-xilinx-linux-g++)
+
+set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(CMAKE_C_FLAGS "--sysroot=${CMAKE_SYSROOT} ${CMAKE_C_FLAGS}")
+set(CMAKE_CXX_FLAGS "--sysroot=${CMAKE_SYSROOT} ${CMAKE_CXX_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS "--sysroot=${CMAKE_SYSROOT} ${CMAKE_EXE_LINKER_FLAGS}")
