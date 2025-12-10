96ed1de1618386e3b140387b1a59dd7d98bc57f7
Daniel Pommer <daniel.pommer@th-nuernberg.de>
2025-12-10 15:05:13 +0100

    added xtensor to cvmat and reverse

--- include/conversions.h
+++ include/conversions.h
@@ -6,6 +6,8 @@
 
 #include "datatypes.h"
 #include <opencv2/core.hpp>
+#include <xtensor/containers/xadapt.hpp>
+
 
 using namespace Eigen;
 
@@ -20,18 +22,21 @@ MatrixXfRowMajor Tensor2Matrix(const Tensor2f& input);
 
 Tensor2f Matrix2Tensor(const MatrixXfRowMajor& input);
 
-
 cv::Mat eigenToCvMat(const MatrixXfRowMajor& eigen_matrix);
-
 
 cv::Mat eigenToCvMatCopy(const MatrixXfRowMajor& eigen_matrix);
 
+cv::Mat xtensorToCvMat(const xt::xtensor<float, 2>& tensor);
+
+cv::Mat xtensorToCvMat(const xt::xtensor<float, 3>& tensor);
 
 MatrixXfRowMajor cvMatToEigen(const cv::Mat& mat);
 
-
 MatrixXfRowMajor cvMatToEigenCopy(const cv::Mat& mat);
 
+xt::xtensor<float, 2> cvMatToV(const cv::Mat& cv_image, int neutral = 128, float contribution = 10.0);
+
+xt::xtensor<float, 2> cvMatToI(const cv::Mat& cv_image);
 
 cv::Mat convertTofloat(cv::Mat& mat);
 #endif //INTERACTINGMAPS_CONVERSIONS_H
